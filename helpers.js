700eadca0168ccbeb33efa924c53c479b7dd6abc
Jason Turner <jason@emptycrate.com>
2015-12-21 16:08:11 -0700

    Add missing helpers.js

---
+++ helpers.js
@@ -0,0 +1,28 @@
+
+function get_baseline(commit)
+{
+  // baseline_repository
+  //
+
+  var baseline_branch = function(){
+    if (commit.branch == "develop") {
+      return "master";
+    } else if (commit.branch == "master") {
+      return null;
+    } else {
+      return "develop";
+    }
+  }();
+
+  if (baseline_branch == null) {
+    return null;
+  }
+
+  if (commit.branch_name == null) {
+    return null;
+  }
+
+  var filtered_data = data.filter(function(o) { return o.branch_name != null && o.branch_name == baseline_branch && o.device_id == commit.device_id});
+
+  return (filtered_data.length == 0)?null:filtered_data[0];
+}
