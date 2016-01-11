11b10f9ff0e4a07a1be8518fa45697c232d598df
Jason Turner <jason@emptycrate.com>
2016-01-11 13:10:09 -0700

    Don't baseline against pending

--- helpers.js
+++ helpers.js
@@ -22,7 +22,7 @@ function get_baseline(commit)
     return null;
   }
 
-  var filtered_data = data.filter(function(o) { return o.branch_name != null && o.branch_name == baseline_branch && o.device_id == commit.device_id});
+  var filtered_data = data.filter(function(o) { return o.branch_name != null && o.branch_name == baseline_branch && o.device_id == commit.device_id && o.pending == false});
 
   return (filtered_data.length == 0)?null:filtered_data[0];
 }
